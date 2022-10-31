4093279c74aa83f2d1cfad95241ce0b9ffe1137b
Praharsh <108566216+PraharshG@users.noreply.github.com>
2022-10-31 17:35:19 +0530

    Update mqtt_subscriber.py

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -1,13 +1,17 @@
+#importing the necessary packages
 import paho.mqtt.client as mqtt
 import time
 
+#a function to print the received message with client data and message received
 def on_message(client,userdata,message):
 	print("Recieved message: ",str(message.payload.decode("utf-8")))
 
+#setting up the ip address and port of the server to communicate
 mqttBroker = "192.168.64.2"
 client = mqtt.Client("Smartphone")
 client.connect(mqttBroker, 1883)
 
+#subscribing to the topic temperature
 client.loop_start()
 client.subscribe("TEMPERATURE")
 client.on_message = on_message
