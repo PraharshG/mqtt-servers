fb50c990783e0107100c920b41bfe51f9afea0d6
Praharsh <108566216+PraharshG@users.noreply.github.com>
2022-10-31 17:32:31 +0530

    Update mqtt_publisher.py

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,11 +1,14 @@
+#importing the necessary packages
 import paho.mqtt.client as mqtt
 from random import randrange, uniform
 import time
 
+#setting up the ip address and port of the server
 mqttBroker = "192.168.64.2"
 client = mqtt.Client("Temperature_Inside")
 client.connect(mqttBroker, 1883)
 
+#passing a random number to topic temperature every 5 seconds
 while True:
     randNumber = uniform(20.0, 21.0)
     client.publish("TEMPERATURE", randNumber)
